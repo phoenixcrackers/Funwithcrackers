42c8195e5a8dbb0e49878d6cf19258ebabc6ecb2
Gokul Kannan P <gokul8506@gmail.com>
2025-06-23 13:00:46 +0530

    first commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+export default function Home() {
+  return (
+    <div>
+        <div className='flex justify-center'>
+            <p className='text-4xl font-bold'>Fun With Crackers</p>
+        </div>
+    </div>
+  )
+}
