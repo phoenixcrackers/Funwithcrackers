4b3839b4c1e4dab4bed510f795d60d8ed4a20594
phoenixcrackers <phoenixcrackersfwc@gmail.com>
2025-07-31 18:19:47 +0530

    uu

--- Config.jsx
+++ Config.jsx
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://funwithcrackers-backend-production.up.railway.app';
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://funwithcrackers-backend-production.up.railway.app';
+// export const API_BASE_URL = 'http://localhost:5000';
