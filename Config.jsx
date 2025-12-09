4491ba99710b2936e784dd07704a7a1b83c3cb4e
phoenixcrackers <phoenixcrackersfwc@gmail.com>
2025-12-09 15:13:19 +0530

    Update Config.jsx

--- Config.jsx
+++ Config.jsx
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = 'https://fwcbackend.funwithcrackers.com';
+export const API_BASE_URL = 'https://fwcbackend.funwithcrackers.com';
 // export const API_BASE_URL = 'http://localhost:5000';
-export const API_BASE_URL = 'https://funwithcrackers-backend-30cd.onrender.com'
+// export const API_BASE_URL = 'https://funwithcrackers-backend-30cd.onrender.com'
